5653ebcf8e5e3141e7c9ef32ffae9881253139d0
Josh <josh.espinas@xero.com>
2020-02-27 20:41:30 +1300

    Initialised Django project

---
+++ ygoapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ygoapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ygoapi.settings')
+
+application = get_asgi_application()
